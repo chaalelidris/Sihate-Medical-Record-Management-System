1f30d475afce3786233547fa59f3db7022d5c75f
Idris Chaalel <71793123+chaalelidris@users.noreply.github.com>
2023-04-21 00:38:42 +0100

    feat:show doctors in index

--- home_page/views.py
+++ home_page/views.py
@@ -1,4 +1,3 @@
-
 from django.contrib import messages
 
 from django.core.mail import EmailMessage
@@ -6,6 +5,7 @@ from django.conf import settings
 from django.shortcuts import redirect, render
 
 from .forms import ContactusForm
+from users import models as users_modals
 
 # -------------------------------------------------------------------------------
 # -------------for checking user is doctor , patient or admin(by sumit)----------
@@ -16,4 +16,8 @@ from .forms import ContactusForm
 # ------------------------ Home view and Contact Form ---------------------------
 # -------------------------------------------------------------------------------
 def home_view(request):
-    return render(request, "index/index.html")
+    doctors = users_modals.Doctor.objects.order_by("-id")[:4]
+    context = {
+        "doctors": doctors,
+    }
+    return render(request, "index/index.html", context)
